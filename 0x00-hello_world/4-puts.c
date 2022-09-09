081f9e21ff7efafcbaa1e92449514c6a533da0da
dyingimmortals1 <pillzcrz@gmail.com>
2022-09-08 20:36:25 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - print the string in the put function
  *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: Always 0 (Success)
  */int main(void)
 {
