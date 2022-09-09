2924205346807449c7d08b62bfef2d501df02b85
dyingimmortals1 <pillzcrz@gmail.com>
2022-09-08 20:20:16 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 #include <stdio.h>
 /**
  * main - Entry point
@@ -6,6 +6,6 @@
  * Return: Always 0 (Success)
  */int main(void)
 {
-	printf("Programming is like building a multilingual puzzle,\n");
+	puts("Programming is like building a multilingual puzzle,\n");
 	return (0);
 }
